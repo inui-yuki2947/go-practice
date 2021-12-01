c2d3a5a1bd1a3b3c78b7b61eaa0756e7ed0bcb00
Inui Yuki <inuiyuki@team-lab.com>
2021-12-01 15:41:28 +0900

    mainパッケージにファイルを追加

--- zoo/main.go
+++ zoo/main.go
@@ -4,6 +4,7 @@ import "fmt"
 import "./animals"
 
 func main() {
+	fmt.Println(AppName())
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.MonkeyFeed())
 	fmt.Println(animals.RabbitFeed())
