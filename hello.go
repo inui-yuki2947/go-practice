7bafd297c9727ad37fd88ae8ad64b32f1617ba04
Inui Yuki <inuiyuki@team-lab.com>
2021-12-01 17:24:28 +0900

    変数を扱うサンプルを追加

--- hello.go
+++ hello.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// 変数定義サンプル
+	var a1 string
+	a1 = "A1"
+	a2 := "A2"
+	var (
+		a3 = 1
+		a4 = 2
+	)
+	fmt.Println(a1, a2, a3, a4)
 }
