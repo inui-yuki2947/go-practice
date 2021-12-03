2fd9c9decda582884ec3714b9e92d86222eb32fa
Inui Yuki <inuiyuki@team-lab.com>
2021-12-03 12:53:58 +0900

    関数を扱うサンプルを追加

--- hello.go
+++ hello.go
@@ -23,8 +23,11 @@ func main() {
 	fmt.Println(b1, b2)
 
 	fmt.Println("-----関数定義サンプル-----")
+	var c3 func(int, string) int
 	fmt.Println(c1(2, 3))
+	fmt.Println(c2)
 	fmt.Println(c2())
+	fmt.Println(c3)
 
 	fmt.Println("-----配列・スライスサンプル-----")
 	var (
