cc7bcd3cb76ba9bf0b26799fe0f61f0e99daec32
Inui Yuki <inuiyuki@team-lab.com>
2022-04-06 18:25:09 +0900

    go-diffのサンプルで、ファイルを読むこむ例を追加

--- go-diff/main.go
+++ go-diff/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/sergi/go-diff/diffmatchpatch"
+	"io/ioutil"
 )
 
 func main() {
+	// 単純な比較
 	dmp := diffmatchpatch.New()
 	diffs := dmp.DiffMain("あああ", "いいい", false)
+	fmt.Println(diffs)
+
+	// ファイルを読み込んで比較
+	text1, _ := ioutil.ReadFile("text/text1.txt")
+	text2, _ := ioutil.ReadFile("text/text2.txt")
+	diffs = dmp.DiffMain(string(text1), string(text2), false)
+	fmt.Println("--------------------------------------------------")
 	fmt.Println(diffs)
 }
